ccfc02a480e0d3ca71897c4678e9653093fd65e7
LilNiclas <113116068+LilNiclas@users.noreply.github.com>
2023-02-06 13:52:47 +0000

    Movies, Animals and Movies have been categorized

---
+++ fix-samples.sh
@@ -0,0 +1,10 @@
+ #!/bin/bash
+ 
+ cd $CODESPACE_VSCODE_FOLDER/samples/
+ mv animals/countries/ .
+ mv countries/movies/ .
+ mv animals/Monkies.txt animals/Monkeys.txt 
+ echo Monkeys > animals/Monkeys.txt 
+ mv movies/Germany.txt countries/
+ mv countries/.apes.txt animals/gh 
+ cd -
